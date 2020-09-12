30662c50f32c3d89763dc1900c45ad20a908086d
Wagner Ribeiro Elias <wagnerre@gmail.com>
2020-09-12 12:11:53 -0300

    Adicionado suport ngrok

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, jsonify
+from flask_ngrok import run_with_ngrok
 import datetime
 import os
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
 @app.route('/', methods=['GET'])
 def index():
     return f"<h1>Welcome to our Data Service! <br> {str(datetime.datetime.today())} </h1>"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    try:
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
+    except:
+        app.run()
+    
